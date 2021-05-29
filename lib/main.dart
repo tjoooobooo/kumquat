bd747e523e09fc24761cf94d528fae0c38a612ed
mdersuneli <muhammed.dersuneli@mycontrol.de>
2021-05-29 11:42:03 +0200

    formular implementiert

--- lib/main.dart
+++ lib/main.dart
@@ -13,19 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
@@ -35,16 +23,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -52,68 +30,135 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  final _formKey = GlobalKey<FormState>();
+  final marktIdController = TextEditingController();
+  final artiktelIdController = TextEditingController();
+  final artikelController = TextEditingController();
+  final preisController = TextEditingController();
+  final preisPfandController = TextEditingController();
+  final preisEinheitController = TextEditingController();
 
   void _incrementCounter() {
+    if (_formKey.currentState.validate()) {
+      print("Formular ist gültig und kann verarbeitet werden");
+      // Hier können wir mit den geprüften Daten aus dem Formular etwas machen.
+      print(marktIdController.text);
+    } else {
+      print("Formular ist nicht gültig");
+    }
     setState(() async {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-
-      FirebaseFirestore firestore = FirebaseFirestore.instance;
-
-      var users = await firestore.collection('users').get();
-      print(users);
+      FirebaseFirestore.instance
+          .collection('users')
+          .get()
+          .then((QuerySnapshot querySnapshot) {
+        querySnapshot.docs.forEach((doc) {
+          print(doc["name"]);
+        });
+      });
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      body: Form(
+        key: _formKey,
+        child: Padding(
+          padding: const EdgeInsets.all(50),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              TextFormField(
+                keyboardType: TextInputType.text,
+                autocorrect: false,
+                controller: marktIdController,
+                decoration: InputDecoration(
+                  labelText: 'Filiale',
+                ),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Bitte die entsprechende Filiale eintragen';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 20),
+              TextFormField(
+                keyboardType: TextInputType.text,
+                autocorrect: false,
+                controller: artiktelIdController,
+                decoration: InputDecoration(
+                  labelText: 'Artikel-Nr',
+                ),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Bitte eine ArtikelNr eintragen';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 20),
+              TextFormField(
+                keyboardType: TextInputType.text,
+                autocorrect: false,
+                controller: artikelController,
+                decoration: InputDecoration(
+                  labelText: 'Artikel',
+                ),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Bitte einen Artikel eintragen';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 20),
+              TextFormField(
+                keyboardType: TextInputType.text,
+                autocorrect: false,
+                controller: preisController,
+                decoration: InputDecoration(
+                  labelText: 'Preis',
+                ),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Bitte einen Preis eintragen';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 20),
+              TextFormField(
+                keyboardType: TextInputType.text,
+                autocorrect: false,
+                controller: preisEinheitController,
+                decoration: InputDecoration(
+                  labelText: 'Einheitspreis',
+                ),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Bitte den Preis pro entprechender Einheit eintragen';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 20),
+              TextFormField(
+                keyboardType: TextInputType.text,
+                autocorrect: false,
+                controller: preisEinheitController,
+                decoration: InputDecoration(
+                  labelText: 'Pfand (nur falls Vorhanden)',
+                ),
+              ),
+            ],
+          ),
         ),
       ),
+
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
