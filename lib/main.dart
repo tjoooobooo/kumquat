a5bcee12a2fd46418002c6636d01fd57fc8b336a
QaboneLama <thomas.schwabauer@gmx.net>
2021-05-30 20:09:43 +0200

    test deploy

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Test Datenbank Uploader',
+      title: 'Alex stinkts Datenbank Uploader',
       theme: ThemeData(
         primarySwatch: Colors.orange,
         visualDensity: VisualDensity.adaptivePlatformDensity,
